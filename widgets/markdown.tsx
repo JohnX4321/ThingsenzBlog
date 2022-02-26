6ba5f2beadcf8cc684fed52dabc9e3c3c02c5bb3
JohnX4321 <johnx4321gaming@gmail.com>
2022-02-26 22:08:50 +0530

    Fixed code bug in markdown

--- widgets/markdown.tsx
+++ widgets/markdown.tsx
@@ -10,21 +10,19 @@ interface MarkdownProps {
 }
 
 const Markdown: FunctionComponent<MarkdownProps> = ({ content }) => {
-    const components: Partial<NormalComponents & SpecialComponents> = {
-        code({node,inline,className,children,...props}) {
-            const match = /language-(\w+)/.exec(className||'');
-            return (!inline&&match)? (
-                <SyntaxHighlighter style={materialLight} PreTag="div" language={match[1]} children={String(children).replace(/\n$/,'')} {...props} />
-            ): (
-                <code className={className ? className:""} {...props}>
-                {children}
-                </code>
-            )
-        }
-        
-    }
     return <div className="markdown-body">
-        <ReactMarkdown components={components} children={content} />
+        <ReactMarkdown children={content} components={{
+                code({node,inline,className,children,...props}) {
+                    const match = /language-(\w+)/.exec(className||'');
+                    return (!inline&&match)? (
+                        <SyntaxHighlighter style={materialLight} PreTag="div" language={match[1]} children={String(children).replace(/\n$/,'')} {...props} />
+                    ): (
+                        <code className={className ? className:""} {...props}>
+                        {children}
+                        </code>
+                    )
+                }
+        }}  />
     </div>
 }
 
